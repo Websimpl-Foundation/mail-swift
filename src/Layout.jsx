8514ea18dbe3c21cfb6ab08b483934be4167a0e2
Bhavya <vermabhavya783@gmail.com>
2024-06-04 19:22:21 +0530

    layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,8 +1,6 @@
-import React, { useState } from "react";
-import { Outlet } from "react-router-dom";
+
 import Navbar from "./components/Navigation/Navbar";
-import { Button } from "@/components/ui/button";
-import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar";
+
 import Sidepanel from "./components/Navigation/Sidepanel";
 
 
